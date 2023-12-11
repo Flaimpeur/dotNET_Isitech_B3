c06436126ef9d7699bdaff7bad158f32f7f13590
Flaimpeur <114905674+Flaimpeur@users.noreply.github.com>
2023-12-11 11:54:12 +0100

    Correction du petit code demandé dans le cours FAIT

---
+++ Cours/Cours.md
@@ -0,0 +1,19 @@
+# Isitech B3 RPI D - .NET
+
+## Préparation
+
+Avoir installer .NET Core 8.0 ou sup
+
+```bash
+dotnet --version // la version la plus recente installer
+
+dotnet --list-sdks //liste des SDK installés
+```
+
+## Cours
+
+.NET est un framework de dev cross-platform et open-source conçu par Microsoft
+
+## Mes notes :
+
+L'API d'un objet c'est comment on veut que l'objet intéragisse avec le monde extérieur, avec l'utilisateur
---
+++ dev/consoleProject/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace consoleProject;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        Console.WriteLine("Press any key to exist.");
+    }
+
+    // Exo prof
+    public static int Sum(IEnumerable<object> values)
+{
+    var sum = 0;
+    foreach (var item in values)
+    {
+        switch (item)
+        {
+            case 0:
+            break;
+            case int val:
+            sum += val;
+            break;
+            case IEnumerable<object> subList when subList.Any():
+            sum += Sum(subList);
+            break;
+        }
+    }
+    return sum;
+}
+}
+
+
