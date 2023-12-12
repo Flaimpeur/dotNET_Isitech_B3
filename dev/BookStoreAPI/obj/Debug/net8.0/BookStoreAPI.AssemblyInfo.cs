d44eebe5da08f91f4f185695114f4b6bab2f6a3d
Flaimpeur <114905674+Flaimpeur@users.noreply.github.com>
2023-12-12 14:46:21 +0100

    Migration fonctionnel maintenant je fait le CRUD

--- dev/BookStoreAPI/obj/Debug/net8.0/BookStoreAPI.AssemblyInfo.cs
+++ dev/BookStoreAPI/obj/Debug/net8.0/BookStoreAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStoreAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3341315508e5fecfbcaf76b76b5a10d70ff96404")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStoreAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStoreAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
